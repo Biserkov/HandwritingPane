be3524e78aa04d9c20b21779e92c80173ad41fb2
Jordan Biserkov <jordan@biserkov.com>
2015-04-21 15:47:46 +0300

    Program, project and solution files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\TabletTip\1.7");
+        regKey.SetValue("LastUsedModalityWasHandwriting", 1, RegistryValueKind.DWord);
+        foreach (var process in Process.GetProcessesByName("TabTip"))
+            process.Kill();
+        Process.Start(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
+    }
+}
